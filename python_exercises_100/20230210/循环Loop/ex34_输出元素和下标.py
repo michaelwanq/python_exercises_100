73892f35dc1498af6487f32077ffbb5fb661e5ca
michael.wan <michael.wan@ichub.com>
2023-02-10 14:57:41 +0800

    2023-02-10 14：57 ex34 commit

--- python_exercises_100/20230210/循环Loop/ex34_输出元素和下标.py
+++ python_exercises_100/20230210/循环Loop/ex34_输出元素和下标.py
@@ -6,6 +6,13 @@ Item 1 has index 0
 Item 2 has index 1
 Item 3 has index 2
 """
+# 方法一、使用循环取列表的索引和对应的值进行打印
+# a = [1, 2, 3]
+
+# for i in range(0,len(a)):
+#     print(f'Item {a[i]} has index {i}')
+# 方法二、使用enumerate()获得下标和列表元素值对
 a = [1, 2, 3]
-for i in range(0,len(a)):
-    print(f'Item {a[i]} has index {i}')
+
+for index, item in enumerate(a):
+    print(f"Item {item} has index {index}")
