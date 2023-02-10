e44f963958cff5b5ae02912791c6bb8ead357384
michael.wan <michael.wan@ichub.com>
2023-02-11 07:21:16 +0800

    2023-02-11 07:20 ex53 commit

--- python_exercises_100/20230211/循环与文件/ex51_生成字母文件.py
+++ python_exercises_100/20230211/循环与文件/ex51_生成字母文件.py
@@ -2,6 +2,6 @@
 编写代码，生成一个文件p051.txt，每一行是一个英文字母，从a~z
 """
 import string
-with open('p051.txt','w') as file:
+with open('p051.txt', 'w') as file:
     for i in string.ascii_lowercase:
         file.write(i + "\n")
