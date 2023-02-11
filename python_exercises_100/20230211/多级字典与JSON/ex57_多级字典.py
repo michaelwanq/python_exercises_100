9d54a8013c5041876fc443605a903f08f15b9143
michael.wan <michael.wan@ichub.com>
2023-02-11 15:02:04 +0800

    2023-02-11 14:55 ex57 commit

---
+++ python_exercises_100/20230211/多级字典与JSON/ex57_多级字典.py
@@ -0,0 +1,15 @@
+"""
+如下是一个多级字典，也就是说每个字典的KEY的VALUE，也是个数据结构
+怎么访问employees的第二个人，得到他的lastName，即 Smith
+d = {"employees": [{"firstName": "John", "lastName": "Doe"},
+                   {"firstName": "Anna", "lastName": "Smith"},
+                   {"firstName": "Peter", "lastName": "Jones"}],
+     "owners": [{"firstName": "Jack", "lastName": "Petter"},
+                {"firstName": "Jessy", "lastName": "Petter"}]}
+"""
+d = {"employees": [{"firstName": "John", "lastName": "Doe"},
+                   {"firstName": "Anna", "lastName": "Smith"},
+                   {"firstName": "Peter", "lastName": "Jones"}],
+     "owners": [{"firstName": "Jack", "lastName": "Petter"},
+                {"firstName": "Jessy", "lastName": "Petter"}]}
+print(d['employees'][1]['lastName'])
