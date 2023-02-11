3319b52f1f895a92a8933fa65eb20d57dfa02a1a
michael.wan <michael.wan@ichub.com>
2023-02-12 07:07:32 +0800

    2023-02-12 06:52 ex67 commit

---
+++ python_exercises_100/20230212/循环条件控制/ex67_空语句.py
@@ -0,0 +1,15 @@
+"""
+如下代码只能输出一个Hello
+while True:
+    print("Hello")
+    if 2 > 1:
+        break
+    print("Hi")
+只修改break这行代码，让程序交替输出Hello、Hi、Hello、Hi，持续不断
+Hello
+Hi
+Hello
+Hi
+Hello
+...
+"""
