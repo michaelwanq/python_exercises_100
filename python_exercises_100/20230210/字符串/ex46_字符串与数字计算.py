5c22da93f5bd523a63cd5403185ffad0d35bd0f2
michael.wan <michael.wan@ichub.com>
2023-02-13 10:02:21 +0800

    2023-02-13 10：02 ex79 commit

--- python_exercises_100/20230210/字符串/ex46_字符串与数字计算.py
+++ python_exercises_100/20230210/字符串/ex46_字符串与数字计算.py
@@ -5,5 +5,5 @@ age_last_year = age - 1
 print("Last year you were %s." % age_last_year)
 """
 age = input("What's your age? ")
-age_last_year = int(age) - 1 # input函数获取的用户输入类型为字符串类型
+age_last_year = int(age) - 1 # input函数获取的用户输入类型为字符串类型。
 print("Last year you were %s." % age_last_year)
