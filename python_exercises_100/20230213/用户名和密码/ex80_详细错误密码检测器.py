94739e9f6b65bc0f7696d7aa7a926e0d5735f40e
michael.wan <michael.wan@ichub.com>
2023-02-15 06:15:57 +0800

    2023-02-15 6:08 ex80 commit

--- python_exercises_100/20230213/用户名和密码/ex80_详细错误密码检测器.py
+++ python_exercises_100/20230213/用户名和密码/ex80_详细错误密码检测器.py
@@ -5,3 +5,19 @@
 密码至少6位数；
 实现效果如下，如果不满足条件会一直循环，注意会给出多个不满足的信息：
 """
+while True:
+    msgs = []
+    psw = input("请输入密码: ")
+    if not any([i.isdigit() for i in psw]):
+        msgs.append("需要至少一位数字")
+    if not any([i.isupper() for i in psw]):
+        msgs.append("需要至少一位大写字母")
+    if len(psw) < 6:
+        msgs.append("需要至少6位密码")
+    if len(msgs) == 0:
+        print("密码检测通过")
+        break
+    else:
+        print("密码不通过，有如下原因: ")
+        for note in msgs:
+            print("*", note)
